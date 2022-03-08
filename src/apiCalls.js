4c5489ec653885c845da139ccc4bf499b543760b
Stevie Ambroise <s.ambroise@protonmail.com>
2022-03-08 10:22:52 -0700

    Create post fetch call

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,4 +1,15 @@
-export const getOrders = () => {
+const getOrders = () => {
   return fetch('http://localhost:3001/api/v1/orders')
       .then(response => response.json())
 }
+
+const postOrder = (newOrder) => {
+  return fetch('http://localhost:3001/api/v1/orders', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json'},
+    body: JSON.stringify(newOrder)
+  })
+  .then(response => response.json())
+}
+
+export {getOrders, postOrder}
