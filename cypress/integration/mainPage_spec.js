d39a375722c4c9dab681176851171fae0523dcc4
Stevie Ambroise <s.ambroise@protonmail.com>
2022-03-08 10:41:43 -0700

    Create main page testing file

---
+++ cypress/integration/mainPage_spec.js
@@ -0,0 +1,14 @@
+context('Assertions', () => {
+  beforeEach(() => {
+    cy.visit('https://example.cypress.io/commands/assertions')
+  })
+
+  //user should see page title
+  //user should see input field
+  //user should see a list of buttons (how would I account for the list of buttons?)
+  //User should be able to type in input field
+  //User should be able to click as many buttons as possible, and see the order thing change
+  //user should be able to click submit button, and see the list of orders change
+  //user should be able to change the amount of orders on the page, add to it
+  //User should be able to see the background image
+})
